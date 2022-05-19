d926bf3d659fde014642489bd9a23e37cc830bc4
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2022-05-19 23:24:57 +0800

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+import cors from "cors;";
 const authRoutes = require("./routes/routes");
 require("dotenv").config();
 require("./config/db.js");
@@ -6,7 +7,7 @@ require("./config/db.js");
 const bodyParser = require("express").json();
 
 app.use(bodyParser);
-
+app.use(cors());
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
